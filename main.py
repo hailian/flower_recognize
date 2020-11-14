779851f6cf3d8d5163992b98f447bebc52cc8233
debdev <abc@abc.com>
2020-11-14 20:22:50 +0800

    炼丹完成

--- main.py
+++ main.py
@@ -15,36 +15,8 @@ import copy
 import json
 from PIL import Image
 
-data_dir = './flower_data/'
-train_dir = data_dir + '/train'
-valid_dir = data_dir + '/valid'
-
-data_transforms = {
-    'train': transforms.Compose([transforms.RandomRotation(45),#随机旋转，-45到45度之间随机选
-        transforms.CenterCrop(224),#从中心开始裁剪
-        transforms.RandomHorizontalFlip(p=0.5),#随机水平翻转 选择一个概率概率
-        transforms.RandomVerticalFlip(p=0.5),#随机垂直翻转
-        transforms.ColorJitter(brightness=0.2, contrast=0.1, saturation=0.1, hue=0.1),#参数1为亮度，参数2为对比度，参数3为饱和度，参数4为色相
-        transforms.RandomGrayscale(p=0.025),#概率转换成灰度率，3通道就是R=G=B
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])#均值，标准差
-    ]),
-    'valid': transforms.Compose([transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
-
-batch_size = 4
-
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'valid']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True) for x in ['train', 'valid']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'valid']}
-class_names = image_datasets['train'].classes
-
-with open('cat_to_name.json', 'r') as f:
-    cat_to_name = json.load(f)
+# 模型保存
+filename='checkpoint.pth'
 
 def im_convert(tensor):
     """ 展示数据"""
@@ -57,38 +29,11 @@ def im_convert(tensor):
 
     return image
 
-fig=plt.figure(figsize=(20, 12))
-columns = 2
-rows = 2
-
-dataiter = iter(dataloaders['valid'])
-inputs, classes = dataiter.next()
-
-for idx in range (columns*rows):
-    ax = fig.add_subplot(rows, columns, idx+1, xticks=[], yticks=[])
-    ax.set_title(cat_to_name[str(int(class_names[classes[idx]]))])
-    plt.imshow(im_convert(inputs[idx]))
-plt.show()
-
-model_name = 'resnet'  #可选的比较多 ['resnet', 'alexnet', 'vgg', 'squeezenet', 'densenet', 'inception']
-#是否用人家训练好的特征来做
-feature_extract = True 
-
-train_on_gpu = torch.cuda.is_available()
-
-if not train_on_gpu:
-    print('CUDA is not available.  Training on CPU ...')
-else:
-    print('CUDA is available!  Training on GPU ...')
-    
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def set_parameter_requires_grad(model, feature_extracting):
     if feature_extracting:
         for param in model.parameters():
             param.requires_grad = False
-
-model_ft = models.resnet152()
 
 def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
     # 选择合适的模型，不同模型的初始化方法稍微有点区别
@@ -160,34 +105,6 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=Tr
         exit()
 
     return model_ft, input_size
-
-model_ft, input_size = initialize_model(model_name, 102, feature_extract, use_pretrained=True)
-
-#GPU计算
-model_ft = model_ft.to(device)
-
-# 模型保存
-filename='checkpoint.pth'
-
-# 是否训练所有层
-params_to_update = model_ft.parameters()
-print("Params to learn:")
-if feature_extract:
-    params_to_update = []
-    for name,param in model_ft.named_parameters():
-        if param.requires_grad == True:
-            params_to_update.append(param)
-            print("\t",name)
-else:
-    for name,param in model_ft.named_parameters():
-        if param.requires_grad == True:
-            print("\t",name)
-
-# 优化器设置
-optimizer_ft = optim.Adam(params_to_update, lr=1e-2)
-scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)#学习率每7个epoch衰减成原来的1/10
-#最后一层已经LogSoftmax()了，所以不能nn.CrossEntropyLoss()来计算了，nn.CrossEntropyLoss()相当于logSoftmax()和nn.NLLLoss()整合
-criterion = nn.NLLLoss()
 
 def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_inception=False,filename=filename):
     since = time.time()
@@ -291,42 +208,6 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_ince
     model.load_state_dict(best_model_wts)
     return model, val_acc_history, train_acc_history, valid_losses, train_losses, LRs 
 
-model_ft, val_acc_history, train_acc_history, valid_losses, train_losses, LRs  = train_model(model_ft, dataloaders, criterion, optimizer_ft, num_epochs=20, is_inception=(model_name=="inception"))
-
-
-for param in model_ft.parameters():
-    param.requires_grad = True
-
-# 再继续训练所有的参数，学习率调小一点
-optimizer = optim.Adam(params_to_update, lr=1e-4)
-scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
-
-# 损失函数
-criterion = nn.NLLLoss()
-
-# Load the checkpoint
-
-checkpoint = torch.load(filename)
-best_acc = checkpoint['best_acc']
-model_ft.load_state_dict(checkpoint['state_dict'])
-optimizer.load_state_dict(checkpoint['optimizer'])
-#model_ft.class_to_idx = checkpoint['mapping']
-
-model_ft, val_acc_history, train_acc_history, valid_losses, train_losses, LRs  = train_model(model_ft, dataloaders, criterion, optimizer, num_epochs=10, is_inception=(model_name=="inception"))
-
-model_ft, input_size = initialize_model(model_name, 102, feature_extract, use_pretrained=True)
-
-# GPU模式
-model_ft = model_ft.to(device)
-
-# 保存文件的名字
-filename='seriouscheckpoint.pth'
-
-# 加载模型
-checkpoint = torch.load(filename)
-best_acc = checkpoint['best_acc']
-model_ft.load_state_dict(checkpoint['state_dict'])
-
 def process_image(image_path):
     # 读取测试数据
     img = Image.open(image_path)
@@ -371,40 +252,164 @@ def imshow(image, ax=None, title=None):
     ax.set_title(title)
     
     return ax
+ 
 
-image_path = 'image_06621.jpg'
-img = process_image(image_path)
-imshow(img)
+if __name__ == '__main__':
+    data_dir = './flower_data/'
+    train_dir = data_dir + '/train'
+    valid_dir = data_dir + '/valid'
 
-img.shape
+    data_transforms = {
+        'train': transforms.Compose([transforms.RandomRotation(45),#随机旋转，-45到45度之间随机选
+            transforms.CenterCrop(224),#从中心开始裁剪
+            transforms.RandomHorizontalFlip(p=0.5),#随机水平翻转 选择一个概率概率
+            transforms.RandomVerticalFlip(p=0.5),#随机垂直翻转
+            transforms.ColorJitter(brightness=0.2, contrast=0.1, saturation=0.1, hue=0.1),#参数1为亮度，参数2为对比度，参数3为饱和度，参数4为色相
+            transforms.RandomGrayscale(p=0.025),#概率转换成灰度率，3通道就是R=G=B
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])#均值，标准差
+        ]),
+        'valid': transforms.Compose([transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
 
-# 得到一个batch的测试数据
-dataiter = iter(dataloaders['valid'])
-images, labels = dataiter.next()
+    batch_size = 4
 
-model_ft.eval()
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'valid']}
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True) for x in ['train', 'valid']}
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'valid']}
+    class_names = image_datasets['train'].classes
 
-if train_on_gpu:
-    output = model_ft(images.cuda())
-else:
-    output = model_ft(images)
+    with open('cat_to_name.json', 'r') as f:
+        cat_to_name = json.load(f)
 
-output.shape
+    fig=plt.figure(figsize=(20, 12))
+    columns = 2
+    rows = 2
 
-_, preds_tensor = torch.max(output, 1)
+    dataiter = iter(dataloaders['valid'])
+    inputs, classes = dataiter.next()
 
-preds = np.squeeze(preds_tensor.numpy()) if not train_on_gpu else np.squeeze(preds_tensor.cpu().numpy())
-preds
+    for idx in range (columns*rows):
+        ax = fig.add_subplot(rows, columns, idx+1, xticks=[], yticks=[])
+        ax.set_title(cat_to_name[str(int(class_names[classes[idx]]))])
+        plt.imshow(im_convert(inputs[idx]))
+    plt.show()
 
-fig=plt.figure(figsize=(20, 20))
-columns =4
-rows = 2
+    model_name = 'resnet'  #可选的比较多 ['resnet', 'alexnet', 'vgg', 'squeezenet', 'densenet', 'inception']
+    #是否用人家训练好的特征来做
+    feature_extract = True 
 
-for idx in range (columns*rows):
-    ax = fig.add_subplot(rows, columns, idx+1, xticks=[], yticks=[])
-    plt.imshow(im_convert(images[idx]))
-    ax.set_title("{} ({})".format(cat_to_name[str(preds[idx])], cat_to_name[str(labels[idx].item())]),
-                 color=("green" if cat_to_name[str(preds[idx])]==cat_to_name[str(labels[idx].item())] else "red"))
-plt.show()
-   
+    train_on_gpu = torch.cuda.is_available()
 
+    if not train_on_gpu:
+        print('CUDA is not available.  Training on CPU ...')
+    else:
+        print('CUDA is available!  Training on GPU ...')
+        
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    model_ft = models.resnet152()
+
+    model_ft, input_size = initialize_model(model_name, 102, feature_extract, use_pretrained=True)
+
+    #GPU计算
+    model_ft = model_ft.to(device)
+
+    # 模型保存
+    #filename='checkpoint.pth'
+
+    # 是否训练所有层
+    params_to_update = model_ft.parameters()
+    print("Params to learn:")
+    if feature_extract:
+        params_to_update = []
+        for name,param in model_ft.named_parameters():
+            if param.requires_grad == True:
+                params_to_update.append(param)
+                print("\t",name)
+    else:
+        for name,param in model_ft.named_parameters():
+            if param.requires_grad == True:
+                print("\t",name)
+
+    # 优化器设置
+    optimizer_ft = optim.Adam(params_to_update, lr=1e-2)
+    scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)#学习率每7个epoch衰减成原来的1/10
+    #最后一层已经LogSoftmax()了，所以不能nn.CrossEntropyLoss()来计算了，nn.CrossEntropyLoss()相当于logSoftmax()和nn.NLLLoss()整合
+    criterion = nn.NLLLoss()
+
+#    model_ft, val_acc_history, train_acc_history, valid_losses, train_losses, LRs  = train_model(model_ft, dataloaders, criterion, optimizer_ft, num_epochs=20, is_inception=(model_name=="inception"))
+
+
+    for param in model_ft.parameters():
+        param.requires_grad = True
+
+    # 再继续训练所有的参数，学习率调小一点
+    optimizer = optim.Adam(params_to_update, lr=1e-4)
+    scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+    # 损失函数
+    criterion = nn.NLLLoss()
+
+    # Load the checkpoint
+
+#    checkpoint = torch.load(filename)
+#    best_acc = checkpoint['best_acc']
+#    model_ft.load_state_dict(checkpoint['state_dict'])
+#    optimizer.load_state_dict(checkpoint['optimizer'])
+#    #model_ft.class_to_idx = checkpoint['mapping']
+
+#    model_ft, val_acc_history, train_acc_history, valid_losses, train_losses, LRs  = train_model(model_ft, dataloaders, criterion, optimizer, num_epochs=10, is_inception=(model_name=="inception"))
+
+    model_ft, input_size = initialize_model(model_name, 102, feature_extract, use_pretrained=True)
+
+    # GPU模式
+    model_ft = model_ft.to(device)
+
+    # 保存文件的名字
+    filename='seriouscheckpoint.pth'
+
+    # 加载模型
+    checkpoint = torch.load(filename)
+    best_acc = checkpoint['best_acc']
+    model_ft.load_state_dict(checkpoint['state_dict'])
+
+    #image_path = 'image_06621.jpg'
+    image_path = 'image_05829.jpg'
+    img = process_image(image_path)
+    imshow(img)
+
+    img.shape
+
+    # 得到一个batch的测试数据
+    dataiter = iter(dataloaders['valid'])
+    images, labels = dataiter.next()
+
+    model_ft.eval()
+
+    if train_on_gpu:
+        output = model_ft(images.cuda())
+    else:
+        output = model_ft(images)
+
+    output.shape
+
+    _, preds_tensor = torch.max(output, 1)
+
+    preds = np.squeeze(preds_tensor.numpy()) if not train_on_gpu else np.squeeze(preds_tensor.cpu().numpy())
+    preds
+
+    fig=plt.figure(figsize=(20, 20))
+    columns =2
+    rows = 2
+
+    for idx in range (columns*rows):
+        ax = fig.add_subplot(rows, columns, idx+1, xticks=[], yticks=[])
+        plt.imshow(im_convert(images[idx]))
+        ax.set_title("{} ({})".format(cat_to_name[str(preds[idx])], cat_to_name[str(labels[idx].item())]),
+                    color=("green" if cat_to_name[str(preds[idx])]==cat_to_name[str(labels[idx].item())] else "red"))
+    plt.show()
